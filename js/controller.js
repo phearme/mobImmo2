2561229b7f49707550fd350a85cd16da0eee87c5
R. Cohen <raphael.cohen@gmail.com>
2014-01-10 17:44:45 +0100

    views

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,25 @@
 ﻿/*jslint browser:true*/
 /*global immoClient*/
-var mainapp = angular.module("mainapp", ["ngSanitize"]);
-mainapp.controller("mainCtrl", function mainCtrl($scope) {
+var mainapp = angular.module("mainapp", ["ngSanitize", "ngTouch", "ngRoute"]);
+
+mainapp.config(["$routeProvider", function ($routeProvider) {
 	"use strict";
+	$routeProvider.when("/Departement/:depcode", {
+		templateUrl: "departement.html"
+	}).when("/Departements/", {
+		templateUrl: "departements.html"
+	}).when("/Search/", {
+		templateUrl: "search.html"
+	}).when("/Geoloc/", {
+		templateUrl: "geoloc.html"
+	}).otherwise({
+		templateUrl: "main.html"
+	});
+}]);
+
+mainapp.controller("mainCtrl", ["$scope", "$location", function ($scope, $location) {
+	"use strict";
+
 	$scope.loading = true;
 	$scope.departements = immoClient.departements;
 	$scope.safeApply = function (fn) {
@@ -14,16 +31,49 @@ mainapp.controller("mainCtrl", function mainCtrl($scope) {
 		}
 	};
 
+	$scope.openDepartement = function (depCode) {
+		console.log("openDepartement", depCode);
+		$scope.selectedDepCode = depCode;
+		if ($scope.selectedDepCode) {
+			$location.path("/Departement/" + depCode);
+		} else {
+			$location.path("/");
+		}
+	};
+
+	$scope.setLocation = function (path) {
+		$location.path(path);
+	};
+
+	$scope.getLocation = function () {
+		return $location.path();
+	};
+
+	$scope.getHeaderLabel = function () {
+		var loc = $scope.getLocation(), label = "";
+		if (loc.indexOf("/Departements/") >= 0) {
+			label = "Départements";
+		} else if (loc.indexOf("/Departement/") >= 0 && $scope.selectedDepCode) {
+			label = $scope.departements[$scope.selectedDepCode].nom;
+		} else if (loc.indexOf("/Search/") >= 0) {
+			label = "Recherche";
+		} else if (loc.indexOf("/Geoloc/") >= 0) {
+			label = "Ma Position";
+		}
+		return label;
+	};
+
 	$scope.priceData = JSON.parse(localStorage.getItem("priceData")) || {};
 	immoClient.loadData(function (priceData) {
-		$scope.safeApply(function () {
-			if (priceData) {
-				$scope.priceData = priceData;
-			}
-			$scope.loading = false;
-		});
+		if (priceData) {
+			$scope.priceData = priceData;
+			localStorage.setItem("priceData", JSON.stringify($scope.priceData));
+		}
+		$scope.loading = false;
+		$scope.safeApply();
 	});
-});
+
+}]);
 /*
 document.addEventListener("deviceready", function () {
 	"use strict";
